71b6792f6259829b044abc057abc58d39531e4af
janna-ukoko <149179490+janna-ukoko@users.noreply.github.com>
2024-09-09 18:46:21 +0100

    Movement with Jump Buffer and Coyote Timer

--- Brackeyes Game Jam/Assets/Scripts/Player.cs
+++ Brackeyes Game Jam/Assets/Scripts/Player.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rb;
+
+    [SerializeField] private Transform _groundCheck;
+
+    [SerializeField] private LayerMask _groundLayer;
+
+    [SerializeField] private float _speed = 10f;
+
+    [SerializeField] private float _jumpPower = 10f;
+
+    float _jumpBufferTime = 0.2f;
+
+    float _jumpBufferCounter;
+
+    float _coyoteTime = 0.1f;
+
+    float _coyoteTimeCounter;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        HandleMovement();
+
+        HandleJump();
+    }
+
+
+    void HandleMovement()
+    {
+        var moveX = Input.GetAxis("Horizontal");
+
+        _rb.velocity = new Vector2(_speed * moveX, _rb.velocity.y);
+    }
+
+    void HandleJump()
+    {
+        if (IsGrounded())
+        {
+            _coyoteTimeCounter = _coyoteTime;
+        }
+
+        else
+        {
+            _coyoteTimeCounter -= Time.fixedDeltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            _jumpBufferCounter = _jumpBufferTime;
+        }
+
+        _jumpBufferCounter -= Time.fixedDeltaTime;
+
+        if (_jumpBufferCounter > 0f && _coyoteTimeCounter > 0f)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpPower);
+
+            _coyoteTimeCounter = 0f;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.25f, _groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_groundCheck.position, 0.25f);
     }
 }
